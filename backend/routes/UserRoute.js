5e013baaf53a1b619c15260aa3791bfb3bf65e7c
0xSabdadev <jasonalhilal@gmail.com>
2023-01-15 15:07:23 +0700

    user middleware added

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {getUsers, getUsersById, createUser, updateUser, deleteUser} from '../controllers/Users.js'
+import {verifyUser} from '../middleware/AuthUser.js'
 
 const router = express.Router()
 
-router.get('/users', getUsers)
-router.get('/users/:id', getUsersById)
-router.post('/users', createUser)
-router.patch('/users/:id', updateUser)
-router.delete('/users/:id', deleteUser)
+router.get('/users', verifyUser, getUsers)
+router.get('/users/:id', verifyUser, getUsersById)
+router.post('/users', verifyUser, createUser)
+router.patch('/users/:id', verifyUser, updateUser)
+router.delete('/users/:id', verifyUser, deleteUser)
 
 export default router

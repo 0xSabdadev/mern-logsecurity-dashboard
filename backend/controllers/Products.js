03879bef16d234de0dd94998151050112ffe1381
0xSabdadev <jasonalhilal@gmail.com>
2023-01-16 07:43:29 +0700

    getdatabyId controller added

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,5 +1,6 @@
 import Products from '../models/ProductModel.js'
 import Users from '../models/UserModel.js'
+import {Op} from 'sequelize'
 
 export const getProducts = async (req, res) => {
     try {
@@ -37,7 +38,51 @@ export const getProducts = async (req, res) => {
         res.status(500).json({msg: error.message})
     }
 }
-export const getProductsById = async (req, res) => {}
+export const getProductsById = async (req, res) => {
+    try {
+        //cek product nya ada pa ga
+        const product = await Products.findOne({
+            where: {
+                uuid: req.params.id,
+            },
+        })
+        if (!product) return res.status(404).json({msg: 'Produk tidak ditemukan'})
+        let response
+        if (req.role == 'admin') {
+            response = await Products.findOne({
+                attributes: ['uuid', 'name', 'price'],
+                where: {
+                    id: product.id,
+                },
+                include: [
+                    {
+                        model: Users,
+                        attributes: ['name', 'email'],
+                    },
+                ],
+            })
+        } else {
+            response = await Products.findOne({
+                attributes: ['uuid', 'name', 'price'],
+                where: {
+                    //user tidak bisa mengambol data produk dr admin/selain user itu
+                    //maka yg di get harus where req.userId dan product.id
+                    [Op.and]: [{id: product.id}, {userId: req.userId}],
+                    // userId: req.userId,
+                },
+                include: [
+                    {
+                        model: Users,
+                        attributes: ['name', 'email'],
+                    },
+                ],
+            })
+        }
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+}
 export const createProduct = async (req, res) => {
     const {name, price} = req.body
     try {

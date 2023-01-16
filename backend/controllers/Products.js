88ec50fb326ecc098bf2271f4750b69891404e07
0xSabdadev <jasonalhilal@gmail.com>
2023-01-16 07:04:54 +0700

    getProduct controller added

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -2,19 +2,35 @@ import Products from '../models/ProductModel'
 import Users from '../models/UserModel'
 
 export const getProducts = async (req, res) => {
-    let response
-    //jika admin, bisa melihat semua product
-    //jika user, hanya bisa melihat product yg dia inputkkan
-    if (req.role == 'admin') {
-        response = await Products.findAll({
-            //karena ada relasi product dan user yg dibuat, jgn lipa diinclude
-            include: [
-                {
-                    model: Users,
+    try {
+        let response
+        //jika admin, bisa melihat semua product
+        //jika user, hanya bisa melihat product yg dia inputkkan
+        if (req.role == 'admin') {
+            response = await Products.findAll({
+                //karena ada relasi product dan user yg dibuat, jgn lipa diinclude
+                include: [
+                    {
+                        model: Users,
+                    },
+                ],
+            })
+        } else {
+            response = await Products.findAll({
+                //hanya melihat produk yg diin[ut (beraerti produk yg sesuai uuid nya)]
+                where: {
+                    userId: req.userId,
                 },
-            ],
-        })
-    } else {
+                include: [
+                    {
+                        model: Users,
+                    },
+                ],
+            })
+        }
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
     }
 }
 export const getProductsById = async (req, res) => {}
